65cfc7a524c9c23043dfc881ea8e72dd0f3a2c9e
logan <1627107283@qq.com>
2024-01-08 08:54:07 +0000

    add rune

--- top_inscriptions.py
+++ top_inscriptions.py
@@ -10,7 +10,8 @@ inscriptions = {
     'btc':{
         'brc-20':['ordi','sats'],
         'arc-20':['atom','quark'],
-        'lighting':['treat','trick','nostr']
+        'lighting':['treat','trick','nostr'],
+        'rune': ['cook','psbts']
     },
     'solana':{
         'spl-20': ['sols']
@@ -43,7 +44,9 @@ websites = {
     'treat': 'https://mainnet.nostrassets.com/#/marketplace/listing',
     'trick': 'https://mainnet.nostrassets.com/#/marketplace/listing',
     'nostr': 'https://mainnet.nostrassets.com/#/marketplace/listing',
-    'mans': 'https://manbit.io/market/mans'
+    'mans': 'https://manbit.io/market/mans',
+    'cook': 'https://runealpha.xyz/market',
+    'psbts': 'https://runealpha.xyz/market'
 }
 
 
@@ -199,6 +202,25 @@ def get_mantle_info(data_list):
         data_list.append([ticker, 'mantle', 'mrc-20', price, result['tick']['amount'] * price, "N/A", "N/A", website])
 
 
+def get_rune_info(data_list):
+    btc_price = get_token_price("BTC")
+    sat_price = float(btc_price) / 1e8
+    headers = {
+        "Referer": "https://runealpha.xyz",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Origin": "https://runealpha.xyz",
+        "Accept": "application/json, text/plain, */*"
+    }
+    result = requests.get('https://api.runealpha.xyz/market/runes?limit=10&offset=0&sortBy=volume_24h&sortOrder=DESC', headers=headers).json()['data']
+    dict = {}
+    for i in result['runes']:
+        dict[i['rune_name'].lower()] = i
+    for ticker in inscriptions['btc']['rune']:
+        website = websites.get(ticker, "")
+        price = float(dict[ticker]['floor_price'])
+        data_list.append([ticker,'btc','rune',price*sat_price,float(dict[ticker]['marketcap'])*sat_price,float(dict[ticker]['change_24h']),float(dict[ticker]['total_volume'])*sat_price, website])
+
+
 @lru_cache()
 def get_all_data(_ts):
     data_list = []
@@ -210,6 +232,7 @@ def get_all_data(_ts):
     get_nostr_info(data_list)
     get_arc20_info(data_list)
     get_mantle_info(data_list)
+    get_rune_info(data_list)
     filter_data = []
     for i in data_list:
         filter_data.append({'tick':i[0],'blockchain':i[1],'protocol':i[2],'price':i[3],'fdv':i[4],'24h_change':i[5],'24h_volume':i[6],'website':i[7]})
